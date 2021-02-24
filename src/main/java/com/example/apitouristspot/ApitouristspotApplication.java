ed05549513604cfb6c89d8b431c1faa6910b59a5
dudulive <edurodriguesbraz@hotmail.com>
2021-02-24 10:49:13 -0300

    iniciando a aplicação

---
+++ src/main/java/com/example/apitouristspot/ApitouristspotApplication.java
@@ -0,0 +1,13 @@
+package com.example.apitouristspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitouristspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitouristspotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apitouristspot/ApitouristspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apitouristspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitouristspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
